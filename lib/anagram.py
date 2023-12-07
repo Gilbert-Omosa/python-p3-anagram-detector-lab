79c01b164db220a11a06e8e9aab30a63034f2490
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-12-07 12:53:17 +0300

    Done with Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [w for w in word_list if sorted(w) == sorted(self.word)]
+    
+# Examples
+
+print(Anagram("listen").match(["enlist", "silent", "inlets", "tinsel", "eternal"]))
+print(Anagram("restrain").match(["strainer", "terrains", "trainers", "retrains"]))
+print(Anagram("medical").match(["declaim", "decimal", "claimed", "melodic", "melancholy"]))
+print(Anagram("ranged").match(["garden", "danger", "gander", "grander", "grandar"]))
